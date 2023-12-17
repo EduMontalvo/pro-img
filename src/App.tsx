dbae8a3f8e0d8556f7ead47b6364fe00819d194f
Edu <kaedu18@gmail.com>
2023-12-17 14:31:37 -0500

    Main terminado

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Hero from './components/Hero'
-
+import Main from './components/Main'
+import Multimedia from './components/Multimedia'
 function App() {
 
   return (
     <>
       <Header/>   
-      <Hero/> 
+      <Hero/>
+      <Main/>
+      <Multimedia/>
       <Footer/>  
     </>
   )
