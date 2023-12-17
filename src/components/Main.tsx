64d44873353cadb96d2540d028bc38dbc1834760
Edu <kaedu18@gmail.com>
2023-12-17 14:32:06 -0500

    Main terminado

---
+++ src/components/Main.tsx
@@ -0,0 +1,77 @@
+
+const Main = () => {
+    return (
+        <div className="px-8 pt-14 pb-14">
+            <h2 className="text-3xl pb-6 font-medium">Popular search</h2>
+            <div className="grid grid-cols-4 gap-4">
+                <div className="relative">
+                    <img className="h-96 w-full rounded-3xl" src="https://images.pexels.com/photos/414110/pexels-photo-414110.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+                    <div className="">
+                        <h3 className="text-white absolute top-4 left-8 uppercase font-bold">Fire</h3>
+                        <h2 className="text-white absolute bottom-16 left-8 font-bold">Circle Fire</h2>
+                        <p className="text-white absolute bottom-8 left-8">0 views</p>
+                    </div>
+                </div>
+                <div className="relative">
+                    <img className="h-96 w-full rounded-3xl" src="https://images.pexels.com/photos/3249121/pexels-photo-3249121.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+                    <div className="">
+                        <h3 className="text-white absolute top-4 left-8 uppercase font-bold">Fire</h3>
+                        <h2 className="text-white absolute bottom-16 left-8 font-bold">Circle Fire</h2>
+                        <p className="text-white absolute bottom-8 left-8">0 views</p>
+                    </div>
+                </div>
+                <div className="relative">
+                    <img className="h-96 w-full rounded-3xl" src="https://images.pexels.com/photos/3249091/pexels-photo-3249091.jpeg" alt="" />
+                    <div className="">
+                        <h3 className="text-white absolute top-4 left-8 uppercase font-bold">Fire</h3>
+                        <h2 className="text-white absolute bottom-16 left-8 font-bold">Circle Fire</h2>
+                        <p className="text-white absolute bottom-8 left-8">0 views</p>
+                    </div>
+                </div>
+                <div className="relative">
+                    <img className="h-96 w-full rounded-3xl" src="https://images.pexels.com/photos/1666021/pexels-photo-1666021.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+                    <div className="">
+                        <h3 className="text-white absolute top-4 left-8 uppercase font-bold">Fire</h3>
+                        <h2 className="text-white absolute bottom-16 left-8 font-bold">Circle Fire</h2>
+                        <p className="text-white absolute bottom-8 left-8">0 views</p>
+                    </div>
+                </div>
+                <div className="relative">
+                    <img className="h-96 w-full rounded-3xl" src="https://images.pexels.com/photos/552785/pexels-photo-552785.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+                    <div className="">
+                        <h3 className="text-white absolute top-4 left-8 uppercase font-bold">Fire</h3>
+                        <h2 className="text-white absolute bottom-16 left-8 font-bold">Circle Fire</h2>
+                        <p className="text-white absolute bottom-8 left-8">0 views</p>
+                    </div>
+                </div>
+                <div className="relative">
+                    <img className="h-96 w-full rounded-3xl" src="https://images.pexels.com/photos/534164/pexels-photo-534164.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+                    <div className="">
+                        <h3 className="text-white absolute top-4 left-8 uppercase font-bold">Fire</h3>
+                        <h2 className="text-white absolute bottom-16 left-8 font-bold">Circle Fire</h2>
+                        <p className="text-white absolute bottom-8 left-8">0 views</p>
+                    </div>
+                </div>
+                <div className="relative">
+                    <img className="h-96 w-full rounded-3xl" src="https://images.pexels.com/photos/302549/pexels-photo-302549.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+                    <div className="">
+                        <h3 className="text-white absolute top-4 left-8 uppercase font-bold">Fire</h3>
+                        <h2 className="text-white absolute bottom-16 left-8 font-bold">Circle Fire</h2>
+                        <p className="text-white absolute bottom-8 left-8">0 views</p>
+                    </div>
+                </div>
+                <div className="relative">
+                    <img className="h-96 w-full rounded-3xl" src="https://images.pexels.com/photos/675764/pexels-photo-675764.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+                    <div className="">
+                        <h3 className="text-white absolute top-4 left-8 uppercase font-bold">Fire</h3>
+                        <h2 className="text-white absolute bottom-16 left-8 font-bold">Circle Fire</h2>
+                        <p className="text-white absolute bottom-8 left-8">0 views</p>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Main
---
+++ src/components/Multimedia.tsx
@@ -0,0 +1,15 @@
+
+const Multimedia = () => {
+    return (
+        <>
+            <div className="w-full relative">
+                <img className="h-96 w-full object-cover" src="https://images.pexels.com/photos/772429/pexels-photo-772429.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+                <h2 className="text-white text-5xl absolute top-1/3 left-24">View videos here</h2>
+                <p className="text-white text-xl absolute top-1/2 left-24">Discover the best videos on the web.</p>
+                <button className="text-white text-xl absolute bottom-28 left-24">View Videos</button>
+            </div>
+        </>
+    )
+}
+
+export default Multimedia
